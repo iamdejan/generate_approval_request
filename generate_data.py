3333e3c97cc1e5d80c32fb54acf551be756456cf
Giovanni Dejan <wb-giovannid529582@antfin.com>
2019-10-26 12:48:23 +0700

    Add randomizer

--- generate_data.py
+++ generate_data.py
@@ -1,8 +1,22 @@
 import time
 import json
+import random
+
+golden_notes = [
+    "Lorem ipsum",
+    "Good to go",
+    "Hello world",
+    "It's okay",
+    "Need improvement",
+    "Could be better next time",
+    "This is gold",
+    "Mantap djiwa",
+    "Cool",
+    "Tjakep",
+]
 
 data = {}
 data["timestamp"] = int(round(time.time()))
-data["note"] = "Good to go"
+data["note"] = random.choice(golden_notes)
 
 print(json.dumps(data), end = '')
